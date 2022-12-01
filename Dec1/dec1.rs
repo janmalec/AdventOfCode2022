98ae58d26f87a123ee334845e540ce5223c4ac5c
Jan Malec <jan.malec@protonmail.com>
2022-12-01 11:47:58 +0000

    December 1

--- Dec1/dec1.rs
+++ Dec1/dec1.rs
@@ -2,11 +2,22 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    let mut biggest_deer = 0;
     let mut deer = 0;
+    let mut deer_weights = Vec::new();
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-      n1 = line.unwrap().parse::<i32>().unwrap();
-      println!("Number {}", n1)
+      let linestr = line.unwrap().to_string();
+      if linestr.len() == 0 {
+        deer_weights.push(deer);
+        deer = 0;
+      } else {
+        deer += linestr.parse::<i32>().unwrap();
+      } 
     }
+    deer_weights.sort();
+    let len = deer_weights.len();
+    println!("Deer with most weight {}", deer_weights[len-1]);
+    let top_3_deer : Vec<i32> = deer_weights.drain(len-3..).collect();
+    let top_sum : i32 = top_3_deer.iter().sum();
+    println!("Top 3: {}", top_sum);
   }
