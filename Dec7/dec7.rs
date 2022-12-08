82e264154055c9dfb44bbcef5ea65aaa1a25ed78
Jan Malec <jan.malec@protonmail.com>
2022-12-08 21:01:06 +0100

    Dec 7 inputs

--- Dec7/dec7.rs
+++ Dec7/dec7.rs
@@ -1,12 +1,26 @@
 use std::io::{self, BufRead};
 
+struct directory {
+  name: String,
+  
+}
+
 fn main() {
   let stdin = io::stdin();
-  let mut path = Vec::new();
-  path.push("/".to_string());
+  let mut path : Vec<String> = Vec::new();
+  //path.push("/".to_string());
   for line in stdin.lock().lines() {
     let linestr = line.unwrap().to_string();
     let split: Vec<String> = linestr.split_whitespace().map(str::to_string).collect();
-    println!("{:?}", split);
+    println!("{:?} {:?}", split, path);
+    if split.get(0).unwrap() == &"$" {
+      if split.get(1).unwrap() == &"cd".to_string() {
+        if split.get(2).unwrap() == &"..".to_string() {
+          path.pop();
+        } else {
+          path.push(split.get(2).unwrap().to_string());
+        }
+      }
+    }
   }
 }
